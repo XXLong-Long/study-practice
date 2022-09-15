d3031b66094b0f830abc68f00df48080700ec858
longfover <107462199+longfover@users.noreply.github.com>
2022-09-15 23:24:54 +0800

    test

---
+++ Project1/Project1/1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a = 0, b = 0;
+	scanf("%d %d", &a, &b);
+	printf("ÎÒ°®Äã»ÆÑÅÀö\n");
+	printf("love");
+	printf("%d", a + b);
+	return 0;
+}
